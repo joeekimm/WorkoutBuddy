29102d961d398423b2c04cf8b5e93fcd515d2f84
J6K <jmkang06@gmail.com>
2017-06-14 12:07:29 -0700

    [Feat] -beginning server process

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const parser = require('body-parser');
+
+const server = express();
+
+
+server.listen(PORT, (err) => {
+  return err ? 
+  console.log('Error while connecting to port ', PORT) :
+  console.log('Successfully connected to port ', PORT);
+})
