6c28278cdcd1b13103615d9823e3437cbb0a703b
J6K <jmkang06@gmail.com>
2017-06-30 17:37:25 -0700

    [Fix] - import/export fixes. now working

--- server/router/index.js
+++ server/router/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
 router.use('/users', require('../db/Users/UserRouter'));
+// router.use('/workouts', require('../db/Workout/WorkoutRouter'));
+// router.use('/users', require('./Users'));
 
 module.exports = router;
