2c58e0ceddfca2cbe929515495347ad7d1c5002d
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-07-12 14:26:41 -0700

    [Add] -sidebar styles and icons -matchesa anbd logout button

--- Client/Containers/Dashboard.jsx
+++ Client/Containers/Dashboard.jsx
@@ -16,17 +16,33 @@ class Dashboard extends Component {
           <div className="side-header" />
           <button className="matches">See Matches</button>
           <div className="accomplishments">
-            see accomplishments <br />
-            post accomplishments
+            <button>
+              <i className="fa fa-eye" aria-hidden="true" />
+                  accomplishments
+            </button>
+            <button>
+              <i className="fa fa-dropbox" aria-hidden="true" />
+              accomplishments</button>
           </div>
           <div className="workouts">
-            create workout <br />
-            see workouts
+            <button>
+              <i className="fa fa-upload" aria-hidden="true" />
+              workout</button>
+            <button>
+              <i className="fa fa-eye" aria-hidden="true" />
+              workouts </button>
           </div>
           <div className="profile">
-            profile <br />
-            messages
+            <button>
+              <i className="fa fa-user-circle" aria-hidden="true" />
+              profile
+            </button>
+            <button>
+              <i className="fa fa-comments-o" aria-hidden="true" />
+              messages
+            </button>
           </div>
+          <button className="logout">LOGOUT</button>
         </div>
         <div className="dash-content">
           rest of content
