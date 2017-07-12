015362060ab1556bbb9762b5b8e35d6e30684a05
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-07-11 19:53:51 -0700

    [Add] -some layout for desktop view on dashboard

--- Client/Containers/Dashboard.jsx
+++ Client/Containers/Dashboard.jsx
@@ -11,15 +11,33 @@ class Dashboard extends Component {
   render() {
     const { logout, history } = this.props;
     return (
-      <div>
-        dashboard hoooooor
-        <button onClick={() => {
-          logout(history);
-        }}
-        >logout</button>
+      <div className="dashboard">
+        <div className="side-bar">
+          <div className="side-header" />
+          <button className="matches">See Matches</button>
+          <div className="accomplishments">
+            see accomplishments <br />
+            post accomplishments
+          </div>
+          <div className="workouts">
+            create workout <br />
+            see workouts
+          </div>
+          <div className="profile">
+            profile <br />
+            messages
+          </div>
+        </div>
+        <div className="dash-content">
+          rest of content
+        </div>
       </div>
     );
   }
 }
+{ /* <button onClick={() => {*/ }
+{ /* logout(history);*/ }
+{ /* }}*/ }
+{ /* >logout</button>*/ }
 
 export default connect(null, { logout })(Dashboard);
