a8091e0885c04e365a72e7803e700abd5e4cd66a
Yubin Kim <joeekimm@gmail.com>
2017-07-27 15:17:01 -0700

    merged repo code

--- Client/Components/Workoutinfo.jsx
+++ Client/Components/Workoutinfo.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+// import PersonalityInsightsV3 from 'watson-developer-cloud/personality-insights/v3';
+
+// const personality_insights = new PersonalityInsightsV3({
+//   username: "80ab8395-6efc-4188-a88a-c58f30353f10",
+//   password: "DwOUcTYNy0yZ",
+//   version_date: '2016-10-19'
+// });
 
 
 class Workoutinfo extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      ibmtext: ''
+    }
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+  
+  handleTextChange(event) {
+    this.setState({
+      ibmtext: event.target.value
+    }, console.log(this.state.ibmtext))
+  }
 
   render() {
     return(
-      <div>workout info :)</div>
+      <div>
+        <form>
+          <label>Input 100 words</label>
+          <br/>
+          <textarea type="text"
+            onChange={(event) => this.handleTextChange(event)}/>
+        </form>
+      </div>
     );
   }
 }
